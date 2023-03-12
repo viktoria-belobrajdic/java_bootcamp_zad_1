8e958d6d1c4dccfa099f862547e9268bf9db0a64
vbelobrajdic <viktoria.belobrajdic@gmail.com>
2023-03-12 17:01:13 +0100

    zad 1

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+import animals.Animal;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        Animal animal = new Animal();
+    }
+}
---
+++ src/animals/Animal.java
@@ -0,0 +1,7 @@
+package animals;
+
+public class Animal {
+    public Animal () {
+        System.out.println("Now accessing Animals");
+    }
+}
